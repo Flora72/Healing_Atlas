ba87988ea12102be14befb0898477175a14501b6
Flora72 <zahratheo569@gmail.com>
2025-09-01 22:55:07 +0300

    debug

--- atlas_core/settings.py
+++ atlas_core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.getenv("SECRET_KEY")  
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['healingatlas.onrender.com']
 
 # Installed apps
