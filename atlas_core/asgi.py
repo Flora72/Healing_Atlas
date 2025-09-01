951ae03515512a35bc38f12ed60da0f8627a17f1
Flora72 <zahratheo569@gmail.com>
2025-09-01 21:48:42 +0300

    Rename inner Healing_Atlas to atlas_core for clarity

--- atlas_core/asgi.py
+++ atlas_core/asgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Healing_Atlas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atlas_core.settings')
+
 
 
 application = get_asgi_application()
